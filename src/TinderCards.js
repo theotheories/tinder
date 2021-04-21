35600e5b71dc75e72abc7520b883a6fcbe41d417
theotheories <57911269+theotheories@users.noreply.github.com>
2021-04-22 00:14:08 +0100

    bit more styling, the hardcoded cards are in the database too

--- src/TinderCards.js
+++ src/TinderCards.js
@@ -5,7 +5,8 @@ import TinderCard from "react-tinder-card";
 import "./TinderCards.css";
 
 function TinderCards() {
-  const [people, setPeople] = useState([{
+  const [people, setPeople] = useState([
+    {
       name: "Danny DeVito",
       url: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/88/Danny_DeVito_cropped_and_edited_for_brightness.jpg/440px-Danny_DeVito_cropped_and_edited_for_brightness.jpg",
     },
